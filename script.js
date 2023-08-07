03f6d33bfa20c0b7f99b4b6bbf7543777787d13e
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-08-07 20:22:50 +0200

    Make toggle function more robust

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const ICON = document.querySelectorAll('a.icon');
 
 function changeState(item) {
-  item.classList.toggle('active');
+  (item.classList.contains('active'))
+    ? item.classList.remove('active')
+    : item.classList.add('active');
 }
 
 ICON.forEach(cardIcon => {
