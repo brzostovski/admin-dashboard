6259baab2008e8189444f936323b67251baf5350
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-08-09 16:39:13 +0200

    Add user-name and full name through script

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const ICON = document.querySelectorAll('div.icon');
+const FULL_NAME_HEADER = document.querySelectorAll('.full-name');
+const USER_NAME_HEADER = document.querySelector('.user-name');
+const FULL_NAME = 'John Snow';
+const USER_NAME = '@jsnow';
 
 function changeState(item) {
   (item.classList.contains('active'))
@@ -6,6 +10,17 @@ function changeState(item) {
     : item.classList.add('active');
 }
 
+function addName(container, name) {
+  container.textContent = name;
+}
+
 ICON.forEach(cardIcon => {
   cardIcon.addEventListener('click', () => changeState(cardIcon));
+})
+
+window.addEventListener('load', () => {
+  FULL_NAME_HEADER.forEach(container => {
+    addName(container, FULL_NAME);
+  })
+  addName(USER_NAME_HEADER, `(${USER_NAME})`);
 })
